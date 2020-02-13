a3f8e775074eda55f6bbec957dd24c4df6aae0e1
Christopher Carter <chrisjohncarter123@gmail.com>
2020-02-13 22:22:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,9 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if(phrase.upcase != phrase)
+  if(phrase == "I LOVE YOU GRANDMA!")
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif(phrase.upcase != phrase)
     "HUH?! SPEAK UP, SONNY!"
   else
     "NO, NOT SINCE 1938!"
